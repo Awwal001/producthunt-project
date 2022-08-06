51cf368fa1cf2cae1c048491934845a13fe215fc
Awwal Aderibigbe <mac@macs-MacBook-Pro.local>
2022-08-06 09:10:28 +0100

    update

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT')
+        'NAME': 'producthunt',
+        'USER': 'postgres',
+        "PASSWORD": 'producthunt',
+        "HOST": 'localhost',
+        "PORT": '5433'
     }
 }
 
