3b17356a9e8e7f545bd796e0e6a9e9906d00c3de
Awwal Aderibigbe <mac@macs-MacBook-Pro.local>
2022-08-06 12:39:04 +0100

    PA delpoy

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -89,27 +89,27 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT')
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'producthunt',
-#         'USER': 'postgres',
-#         "PASSWORD": 'producthunt',
-#         "HOST": 'localhost',
-#         "PORT": '5433'
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         "PASSWORD": os.environ.get('DB_PASSWORD'),
+#         "HOST": os.environ.get('DB_HOST'),
+#         "PORT": os.environ.get('DB_PORT')
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthunt',
+        'USER': 'postgres',
+        "PASSWORD": 'producthunt',
+        "HOST": 'localhost',
+        "PORT": '5433'
+    }
+}
 
 # import dj_database_url
 
