19ca2052311e80d85d4afe75ad7904b6bc7b544b
Awwal Aderibigbe <mac@macs-MacBook-Pro.local>
2022-12-22 09:23:24 +0100

    new commits

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -91,26 +91,26 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dc2t74ftcn6tj0',
-        'USER': 'zvwyauqqplzspr',
-        "PASSWORD": 'a78258d601574fe910377a12216e7964da243ef40513ab6d9b5f4d29d383c9ae',
-        "HOST": 'ec2-34-227-135-211.compute-1.amazonaws.com',
-        "PORT": '5432'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dc2t74ftcn6tj0',
+#         'USER': 'zvwyauqqplzspr',
+#         "PASSWORD": 'a78258d601574fe910377a12216e7964da243ef40513ab6d9b5f4d29d383c9ae',
+#         "HOST": 'ec2-34-227-135-211.compute-1.amazonaws.com',
+#         "PORT": '5432'
+#     }
+# }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
